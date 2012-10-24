df08bf8a008bd9105c0714106be4b4bbd2aabcc7
Amal Krishnan <amalkrishnancg@gmail.com>
2012-10-24 14:01:11 -0700

    Fixed a few indentation issues

--- sneaky.py
+++ sneaky.py
@@ -29,64 +29,62 @@ from time import gmtime, strftime
 
 # This class parses the list of recently scanned servers
 class SSLLabsParser(HTMLParser):
-	in_print_block = False
-  
-	def handle_starttag(self, tag, attrs):
-	     if tag == 'div':
-	     		for attr in attrs:
-				if attr == ('class', 'boxHead'):
-					# If this is the second boxHead div, stop parsing
-					if (self.in_print_block == True):
-						#TODO: Find a better way to stop processing						
-						self.reset()	
-					else:
-						self.in_print_block = True
+    in_print_block = False
+    
+    def handle_starttag(self, tag, attrs):
+        if tag == 'div':
+	    for attr in attrs:
+	        if attr == ('class', 'boxHead'):
+		    # If this is the second boxHead div, stop parsing
+		    if (self.in_print_block == True):
+		        #TODO: Find a better way to stop processing						
+			self.reset()	
+		    else:
+		        self.in_print_block = True
+        if tag == 'a' and self.in_print_block:
+	    for attr in attrs:
+	        if attr[0] == 'href': 
+		    if ANALYSIS_PAGE in attr[1]:
+		        # Pick out the server url, url decode it and add to list
+			ContentManager.scanned_servers.add(urllib2.unquote(re.search('.*\?d=(.*)',attr[1]).group(1)))
 					
-   	     if tag == 'a' and self.in_print_block:
-	     	for attr in attrs:
-			if attr[0] == 'href': 
-				if ANALYSIS_PAGE in attr[1]:
-					# Pick out the server url, url decode it and add to list
-					ContentManager.scanned_servers.add(urllib2.unquote(re.search('.*\?d=(.*)',attr[1]).group(1)))
-					
-
 # This class handles HTML parsing and retrieving list of servers and ratings
 class ContentManager():
-	scanned_servers = set([])
-
-	def load_content(self):
-		self.content = urllib2.urlopen(QUERY_LOCATION)
-	
-	def process_HTML_content(self):
-		self.parser = SSLLabsParser()
-		try:	
-			self.parser.feed (self.content.read())
-		except AssertionError: 
-			pass	
+    scanned_servers = set([])
+    
+    def load_content(self):
+        self.content = urllib2.urlopen(QUERY_LOCATION)
+    
+    def process_HTML_content(self):
+	self.parser = SSLLabsParser()
+	try:	
+	    self.parser.feed (self.content.read())
+	except AssertionError: 
+	    pass	
 
 def main():
-	signal.signal(signal.SIGINT, exit_handler)
-	print 'Sneaky is retrieving list of scanned servers (CTRL-C to quit)'
-	manager = ContentManager()
-	while(True):
-		manager.load_content()
-		manager.process_HTML_content()
-		# Print list of servers
-		print "\n--------------" + strftime("%d %b %Y %H:%M:%S", gmtime()) + "--------------"
-		for server in ContentManager.scanned_servers:
-			print server+"\n"
-		time.sleep(QUERY_INTERVAL_SECS)
+    signal.signal(signal.SIGINT, exit_handler)
+    print 'Sneaky is retrieving list of scanned servers (CTRL-C to quit)'
+    manager = ContentManager()
+    while(True):
+        manager.load_content()
+	manager.process_HTML_content()
+	# Print list of servers
+	print "\n--------------" + strftime("%d %b %Y %H:%M:%S", gmtime()) + "--------------"
+	for server in ContentManager.scanned_servers:
+	    print server+"\n"
+	time.sleep(QUERY_INTERVAL_SECS)
 
 # Function writes list of servers to file on termination
 def exit_handler(signum, frame):
     try:
-    	file = open(OUTPUT_FILE, 'w+')
+        file = open(OUTPUT_FILE, 'w+')
    	for server in ContentManager.scanned_servers:
-		file.write(server  + "\n")
+	    file.write(server  + "\n")
     	file.close()
     	sys.exit("\nSneaky successfully wrote to " + OUTPUT_FILE)	
     except IOError:
 	sys.exit("\nSneaky was unable to write to " + OUTPUT_FILE)	
 
 if __name__ == "__main__":
-	main()
+    main()
