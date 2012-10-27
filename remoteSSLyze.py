eedd0ee4284d6d77799fa7c9692f96a7aab55b9c
Amal Krishnan <amalkrishnancg@gmail.com>
2012-10-26 17:21:38 -0700

    added remote server support to remoteSSLyze

--- remoteSSLyze.py
+++ remoteSSLyze.py
@@ -1,48 +1,64 @@
 #!/usr/bin/env python
-#----------------------------------------------------------------------
+#-------------------------------------------------------------
 # remoteSSLyze Server
-# Version: 0.1
+# Version: 0.1a
 # Author: Amal Krishnan (amalkrishnancg@gmail.com)
-# Copyright: None 
+# Copyright: None
 #
 # A service that accepts a URL and tests it with SSLyze
 #
-#-----------------------------------------------------------------------
-
+#-------------------------------------------------------------
+ 
 #------------------------OPTIONS------------------------------
 SSLYZE_PATH = '/home/amal.krishnan/Tools/SSL/sslyze/sslyze.py'
 LISTEN_PORT = 13373
-HOST = 'localhost'
-REMOTE_SERVER_URL = 
+HOST = 'amalkrishnan-wsl1'
+REMOTE_SERVER_URL = '10.0.41.101:13373'
+OPENSSL_V0_9_OPTS = '--sslv3 --tlsv1 --resum --certinfo=basic --hide_rejected_ciphers --http_get'
+OPENSSL_V1_0_OPTS = '--tlsv1_1 --tlsv1_2 --compression --reneg --hide_rejected_ciphers'  
+OPENSSL_VERSION = 0.9
+QUERY_REMOTE_SERVER = True
 #-------------------------------------------------------------
 
+import httplib
 import os
 import re
 import subprocess
-
+ 
 from bottle import run, route, request
 from urlparse import urlparse
-
+ 
 @route('/sslyze')
 def run_sslyze():
     if 'url' not in request.GET.keys():
         return ("The parameter \'url\' not found. Usage is ' + 'protocol://domain:port/sslyze'"
          "?url=scheme://sitetoscan:port' <br /><b>Example: http://sslyzedomain.com:port/sslyze?url="
          "https://www.salesforce.com:443</b>")
-    # Check if scheme was not included by a lazy user. If not, include scheme
+    # Check if scheme was not included by a lazy user. If not inlcuded, add scheme
     if (re.match("\w+://", request.GET['url'])):
         url = request.GET['url']
     else:
         url = 'https://' + request.GET['url']
-    # urlparse() does the validation of the url. Stay secure people! 
+    # urlparse() does the validation of the url. Stay secure people!
     url_parse_object = urlparse(url)
-    print url_parse_object 
     if (url_parse_object.netloc == ''):
         return 'Enter a valid url. Don\'t be naughty'
-    
-    host_option_list = [SSLYZE_PATH, '--sslv3', '--tlsv1', '--resum',  '--certinfo=basic', '--hide_rejected_ciphers', '--http_get',  url_parse_object.netloc]
-    print 'Running sslyze on' + url_parse_object.path
-    host_proc = subprocess.Popen(host_option_list, 0, None, subprocess.PIPE, subprocess.PIPE, None)
-    return  "<br />".join(''.join(host_proc.stdout.readlines()).split("\n")) # reads the output and replaces \n with <br/>
-    
+    if (OPENSSL_VERSION == 0.9):
+        host_option_list = [SSLYZE_PATH] + OPENSSL_V0_9_OPTS.split() +  [url_parse_object.netloc]
+    else:
+        host_option_list = [SSLYZE_PATH] + OPENSSL_V1_0_OPTS.split() +  [url_parse_object.netloc]
+    host_proc = subprocess.Popen(host_option_list, 0, None, subprocess.PIPE, subprocess.PIPE, None)    
+    # reads the output and replaces \n with <br/>
+    output =  "<br />".join(''.join(host_proc.stdout.readlines()).split("\n"))
+    if (QUERY_REMOTE_SERVER):
+        connection = httplib.HTTPConnection(REMOTE_SERVER_URL)
+        connection.request('GET','/sslyze?url=' + url_parse_object.netloc)
+        output_r= connection.getresponse().read()
+        output += '<br />-----------------------------------------------------------'
+        output += '<br /> Results from remote scan <br />'
+        output += '-----------------------------------------------------------'
+        output += "<br />".join(''.join(output_r).split("\n"))
+        connection.close()
+    return output
+
 run(host=HOST, port=LISTEN_PORT)
